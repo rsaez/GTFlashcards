5d51c37fddd74714f5349f9b567e3ef82985dcd2
Wenlei He <fshewl@gmail.com>
2013-10-18 14:37:52 -0400

    addFlashcard(), a POST api

--- gtflashcards.php
+++ gtflashcards.php
@@ -25,6 +25,16 @@
 		header("Content-type: application/json");
 		echo json_encode($result);
 	}
+	
+	function addFlashcard($title,$question,$answer) {
+		$dbQuery = sprintf("INSERT INTO flashcard (title,question,answer) VALUES ('%s','%s','%s')",
+			mysql_real_escape_string($title),
+			mysql_real_escape_string($question),
+			mysql_real_escape_string($answer));
+		$result = getDBResultInserted($dbQuery);
+		header("Content-type: application/json");
+		echo json_encode($result);
+	}
 
 
 ?>
